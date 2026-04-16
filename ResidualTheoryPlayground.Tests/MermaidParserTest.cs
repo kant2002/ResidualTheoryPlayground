918c52252f0a31aa06d64f081841560130fefa9e
Andrii Kurdiumov <kant2002@gmail.com>
2026-04-16 22:41:36 +0200

    Create simple mermaid parser

---
+++ ResidualTheoryPlayground.Tests/MermaidParserTest.cs
@@ -0,0 +1,65 @@
+﻿namespace ResidualTheoryPlayground.Tests;
+
+[TestClass]
+public sealed class MermaidParserTest
+{
+    [TestMethod]
+    public void InvalidDiagramThrowError()
+    {
+        var parser = new MermaidParser();
+        var invalidDiagram = "graph TB\nA-->B\nInvalidLine\nC-->D";
+        Assert.Throws<FormatException>(() => parser.GetBlocks(invalidDiagram));
+    }
+    [TestMethod]
+    public void MissingHeaderDiagramThrowError()
+    {
+        var parser = new MermaidParser();
+        var invalidDiagram = "A-->B\nC-->D";
+        Assert.Throws<FormatException>(() => parser.GetBlocks(invalidDiagram));
+    }
+    [TestMethod]
+    public void EmptyDiagramThrowError()
+    {
+        var parser = new MermaidParser();
+        var invalidDiagram = "";
+        Assert.Throws<FormatException>(() => parser.GetBlocks(invalidDiagram));
+    }
+
+    [TestMethod]
+    public void ParseSimpleUnconnectedDiagram()
+    {
+        var parser = new MermaidParser();
+        var invalidDiagram = "graph TB\nA-->B\nC-->D";
+
+        var components = parser.GetBlocks(invalidDiagram);
+
+        CollectionAssert.AreEquivalent(new string[] { "A", "B", "C", "D" }, components);
+    }
+
+    [TestMethod]
+    public void ParseSimpleConnectedDiagram()
+    {
+        var parser = new MermaidParser();
+        var invalidDiagram = "graph TB\nA-->B\nC-->C";
+
+        var components = parser.GetBlocks(invalidDiagram);
+
+        CollectionAssert.AreEquivalent(new string[] { "A", "B", "C" }, components);
+    }
+
+    [TestMethod]
+    public void ParseNamedNodesConnectedDiagram()
+    {
+        var parser = new MermaidParser();
+        var invalidDiagram =
+            """
+            graph TB
+            A[Name]-->B
+            B-->C["Name with quotes"]
+            """;
+
+        var components = parser.GetBlocks(invalidDiagram);
+
+        CollectionAssert.AreEquivalent(new string[] { "Name", "B", "Name with quotes" }, components);
+    }
+}
---
+++ ResidualTheoryPlayground/MermaidParser.cs
@@ -0,0 +1,48 @@
+﻿namespace ResidualTheoryPlayground;
+
+public class MermaidParser
+{
+    public string[] GetBlocks(string diagramDefinition)
+    {
+        var parts = diagramDefinition.Split(['\n','\r'], StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        if (parts.Length == 0)
+            throw new System.FormatException();
+        if (parts[0].Trim() != "graph TB")
+            throw new System.FormatException();
+        HashSet<string> result = new();
+        Dictionary<string, string> names = new();
+        foreach (var part in parts.Skip(1))
+        {
+            var items = part.Split("-->", StringSplitOptions.TrimEntries);
+            if (items.Length == 1)
+            {
+                throw new System.FormatException();
+            }
+
+            foreach (var component in items)
+            {
+                if (component.Contains("["))
+                {
+                    var bracketIndex = component.IndexOf("[");
+                    var closingBracketIndex = component.LastIndexOf("]");
+                    var name = component[..bracketIndex].Trim();
+                    var displayName = component.Substring(bracketIndex + 1, closingBracketIndex - bracketIndex - 1).Trim('"');
+                    names[name] = displayName;
+                    result.Add(displayName);
+                }
+                else
+                {
+                    if (names.TryGetValue(component, out var displayName))
+                    {
+                        result.Add(displayName);
+                    }
+                    else
+                    {
+                        result.Add(component);
+                    }
+                }
+            }
+        }
+        return result.ToArray();
+    }
+}
