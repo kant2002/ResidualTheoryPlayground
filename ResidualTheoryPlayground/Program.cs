90e1e39a5749c7c5db36c1dc246dca39d463e65b
Andrii Kurdiumov <kant2002@gmail.com>
2026-04-16 22:14:02 +0200

    Create sample page

--- ResidualTheoryPlayground/Program.cs
+++ ResidualTheoryPlayground/Program.cs
@@ -7,5 +7,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<SystemModel>();
+builder.Services.AddMermaidJS();
 
 await builder.Build().RunAsync();
---
+++ ResidualTheoryPlayground/SystemModel.cs
@@ -0,0 +1,28 @@
+﻿namespace ResidualTheoryPlayground;
+
+public class SystemModel
+{
+    public string OriginalModel { get; set; } =
+        """
+        graph TB
+            OR["Order Intake"] --> KQ["Kitchen Queue"]
+            KQ-->PP["Payment Processing"]
+            PP-->PF["Pickup Flow"]
+        """;
+    public string NewModel { get; set; } =
+        """
+        graph TB
+            EM["Express menu"]-->O["Orders (limited)"]
+            O-->DQ[Digital Queue]
+            DQ-->DP[Dual Payment]
+            CF[Cup forward]
+        """;
+
+    public List<string> PlannedStressors { get; set; } = new();
+
+    public List<string> UnplannedStressors { get; set; } = new();
+
+    public List<string> Components { get; set; } = new();
+
+    public bool[,] ResidueMatrix { get; set; } = new bool[0, 0];
+}
